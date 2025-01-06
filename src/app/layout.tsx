7585e9e6854e9cf1d31630d09598bb6a892c28d1
rucorabi <rucorabi@gmail.com>
2025-01-06 13:26:34 +0900

    デザインバグ修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./global.css";
 
 import { CssBaseline } from "@mui/material";
 import { GA_TRACKING_ID } from "../config";
--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -30,88 +30,86 @@ type Props = {
 
 export default function Template({ title, children }: Props) {
   return (
-    <Box
-      sx={{
-        minHeight: "100vh",
-        background:
-          "linear-gradient(135deg, rgba(255,200,200,0.7), rgba(255,255,200,0.7), rgba(200,255,200,0.7), rgba(200,200,255,0.7))",
-      }}
-    >
-      <ThemeProvider theme={theme}>
-        <Box minHeight="100vh" display="flex" flexDirection="column">
-          <MyAppBar text={title} />
-          <Box
-            component="main"
-            flex={1}
-            sx={{
-              py: 2,
-            }}
-          >
-            <Container maxWidth="sm">
-              <Paper
-                elevation={3}
-                sx={{
-                  p: 3,
-                  borderRadius: 2,
-                  bgcolor: "rgba(255,255,255,0.95)",
-                  backdropFilter: "blur(10px)",
-                }}
-              >
-                {children}
-              </Paper>
-              <Typography color="gray" sx={{ mt: 1, textAlign: "center" }}>
-                ※ 本サイトはファンメイドの非公式アプリです
-              </Typography>
-            </Container>
-          </Box>
-          <Box component="footer" sx={{ py: 2 }}>
-            <Grid
-              container
-              spacing={{
-                xs: 1,
-                sm: 2,
-              }}
-              justifyContent="center"
-              alignItems="center"
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          height: "100%",
+        }}
+        display="flex"
+        flexDirection="column"
+      >
+        <MyAppBar text={title} />
+        <Box
+          component="main"
+          flex={1}
+          sx={{
+            py: 2,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Paper
+              elevation={3}
               sx={{
-                flexDirection: {
-                  xs: "column",
-                  sm: "row",
-                },
+                p: 3,
+                borderRadius: 2,
+                bgcolor: "rgba(255,255,255,0.95)",
+                backdropFilter: "blur(10px)",
               }}
             >
-              <Grid>
-                <Typography color="inherit">Created by るこらび.</Typography>
-              </Grid>
-              <Grid height={24}>
-                <Typography
-                  href="/terms/"
-                  component="a"
-                  display="inline-flex"
-                  alignItems="center"
-                  target="_blank"
-                  sx={{
-                    color: "inherit",
-                    textDecoration: "none",
-                    "&:hover": {
-                      // textDecoration: "underline",
-                      borderBottom: "1px solid",
-                    },
-                  }}
-                >
-                  <OpenInNewIcon fontSize="small" sx={{ mr: 1 }} />
-                  利用規約
-                </Typography>
-              </Grid>
-              <Grid>
-                <Typography textAlign="center" color="gray">
-                  © 2025 All rights reserved.
-                </Typography>
-              </Grid>
-            </Grid>
-          </Box>
+              {children}
+            </Paper>
+            <Typography color="gray" sx={{ mt: 1, textAlign: "center" }}>
+              ※ 本サイトはファンメイドの非公式アプリです
+            </Typography>
+          </Container>
         </Box>
-      </ThemeProvider>
-    </Box>
+        <Box component="footer" sx={{ pt: 2, pb: 3 }}>
+          <Grid
+            container
+            spacing={{
+              xs: 1,
+              sm: 2,
+            }}
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              flexDirection: {
+                xs: "column",
+                sm: "row",
+              },
+            }}
+          >
+            <Grid>
+              <Typography color="inherit">Created by るこらび.</Typography>
+            </Grid>
+            <Grid height={24}>
+              <Typography
+                href="/terms/"
+                component="a"
+                display="inline-flex"
+                alignItems="center"
+                target="_blank"
+                sx={{
+                  color: "inherit",
+                  textDecoration: "none",
+                  "&:hover": {
+                    // textDecoration: "underline",
+                    borderBottom: "1px solid",
+                  },
+                }}
+              >
+                <OpenInNewIcon fontSize="small" sx={{ mr: 1 }} />
+                利用規約
+              </Typography>
+            </Grid>
+            <Grid>
+              <Typography textAlign="center" color="gray">
+                © 2025 All rights reserved.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </ThemeProvider>
   );
 }

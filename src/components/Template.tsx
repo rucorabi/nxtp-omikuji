ce2be0dd39f9dc2911fedbc909ff310dd037cbbf
rucorabi <rucorabi@gmail.com>
2025-01-06 11:25:16 +0900

    フッターの追加

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -6,7 +6,11 @@ import {
   createTheme,
   Paper,
   ThemeProvider,
+  Typography,
+  Grid2 as Grid,
 } from "@mui/material";
+
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 
 import { ReactNode } from "react";
 import MyAppBar from "./MyAppBar";
@@ -34,25 +38,78 @@ export default function Template({ title, children }: Props) {
       }}
     >
       <ThemeProvider theme={theme}>
-        <MyAppBar text={title} />
-        <Box
-          sx={{
-            py: 2,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Paper
-              elevation={3}
+        <Box minHeight="100vh" display="flex" flexDirection="column">
+          <MyAppBar text={title} />
+          <Box
+            component="main"
+            flex={1}
+            sx={{
+              py: 2,
+            }}
+          >
+            <Container maxWidth="sm">
+              <Paper
+                elevation={3}
+                sx={{
+                  p: 3,
+                  borderRadius: 2,
+                  bgcolor: "rgba(255,255,255,0.95)",
+                  backdropFilter: "blur(10px)",
+                }}
+              >
+                {children}
+              </Paper>
+              <Typography color="gray" sx={{ mt: 1, textAlign: "center" }}>
+                ※ 本ツールはファンメイドの非公式アプリです
+              </Typography>
+            </Container>
+          </Box>
+          <Box component="footer" sx={{ py: 2 }}>
+            <Grid
+              container
+              spacing={{
+                xs: 1,
+                sm: 2,
+              }}
+              justifyContent="center"
+              alignItems="center"
               sx={{
-                p: 4,
-                borderRadius: 2,
-                bgcolor: "rgba(255,255,255,0.95)",
-                backdropFilter: "blur(10px)",
+                flexDirection: {
+                  xs: "column",
+                  sm: "row",
+                },
               }}
             >
-              {children}
-            </Paper>
-          </Container>
+              <Grid>
+                <Typography color="inherit">Created by るこらび.</Typography>
+              </Grid>
+              <Grid height={24}>
+                <Typography
+                  href="/terms/"
+                  component="a"
+                  display="inline-flex"
+                  alignItems="center"
+                  target="_blank"
+                  sx={{
+                    color: "inherit",
+                    textDecoration: "none",
+                    "&:hover": {
+                      // textDecoration: "underline",
+                      borderBottom: "1px solid",
+                    },
+                  }}
+                >
+                  <OpenInNewIcon fontSize="small" sx={{ mr: 1 }} />
+                  利用規約
+                </Typography>
+              </Grid>
+              <Grid>
+                <Typography textAlign="center" color="gray">
+                  © 2025 All rights reserved.
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
         </Box>
       </ThemeProvider>
     </Box>
